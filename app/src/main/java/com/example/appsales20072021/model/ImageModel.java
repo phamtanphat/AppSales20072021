1105afc4c1a285d13e57084741115e23f69cde3f
phamtanphat <phatdroid94@gmail.com>
2021-11-13 18:17:43 +0700

    food model

---
+++ app/src/main/java/com/example/appsales20072021/model/ImageModel.java
@@ -0,0 +1,12 @@
+package com.example.appsales20072021.model;
+
+public class ImageModel {
+    public String imageUrl;
+
+    @Override
+    public String toString() {
+        return "ImageModel{" +
+                "imageUrl='" + imageUrl + '\'' +
+                '}';
+    }
+}
