7f3f33c6f9c9b66de231635c7bea6c8972562c6c
phamtanphat <phatdroid94@gmail.com>
2021-11-15 11:55:55 +0700

    update ui

--- app/src/main/java/com/example/appsales20072021/view/activity/HomeActivity.java
+++ app/src/main/java/com/example/appsales20072021/view/activity/HomeActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -14,26 +15,43 @@ import com.example.appsales20072021.databinding.ActivityHomeBinding;
 import com.example.appsales20072021.model.FoodModel;
 import com.example.appsales20072021.repository.AuthenRepository;
 import com.example.appsales20072021.repository.FoodRepository;
+import com.example.appsales20072021.view.adapter.FoodAdapter;
 import com.example.appsales20072021.viewmodel.AuthenViewModel;
 import com.example.appsales20072021.viewmodel.FoodViewModel;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     ActivityHomeBinding mBinding;
     FoodViewModel mFoodViewModel;
+    FoodAdapter mFoodAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mBinding = ActivityHomeBinding.inflate(getLayoutInflater());
         setContentView(mBinding.getRoot());
 
-        mFoodViewModel = new ViewModelProvider(this).get(FoodViewModel.class);
-        mFoodViewModel.updateFoodRepository(new FoodRepository());
+
+
+        initData();
 
         observerData();
         event();
+    }
+
+    private void initData() {
+        mFoodViewModel = new ViewModelProvider(this).get(FoodViewModel.class);
+        mFoodViewModel.updateFoodRepository(new FoodRepository());
+
+        mBinding.toolbarHome.setTitle("Food");
+        mBinding.toolbarHome.setTitleTextColor(Color.WHITE);
+
+        mFoodAdapter = new FoodAdapter();
+        mBinding.rcvFood.setHasFixedSize(true);
+        mBinding.rcvFood.setAdapter(mFoodAdapter);
+
     }
 
     private void event() {
@@ -44,20 +62,18 @@ public class HomeActivity extends AppCompatActivity {
         mFoodViewModel.getFoodsModel().observe(this, new Observer<List<FoodModel>>() {
             @Override
             public void onChanged(List<FoodModel> foodModels) {
-                for (int i = 0; i < foodModels.size(); i++) {
-                    Log.d("BBB",foodModels.get(i).toString());
-                }
+                mFoodAdapter.updateListFoodModels(foodModels);
             }
         });
 
         mFoodViewModel.getLoading().observe(this, new Observer<Boolean>() {
             @Override
             public void onChanged(Boolean isLoading) {
-//                if (isLoading) {
-//                    mBinding.containerLoading.layoutLoading.setVisibility(View.VISIBLE);
-//                } else {
-//                    mBinding.containerLoading.layoutLoading.setVisibility(View.GONE);
-//                }
+                if (isLoading) {
+                    mBinding.containerLoading.layoutLoading.setVisibility(View.VISIBLE);
+                } else {
+                    mBinding.containerLoading.layoutLoading.setVisibility(View.GONE);
+                }
             }
         });
 
